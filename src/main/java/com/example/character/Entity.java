c3af8184c08c9166e7a6a282045f15bc491be9fd
Dan-Mihai Rusu <rusu.danmihai12@gmail.com>
2024-10-17 22:13:06 +0300

    Initial commit

---
+++ src/main/java/com/example/character/Entity.java
@@ -0,0 +1,58 @@
+package com.example.character;
+
+import javafx.geometry.Point2D;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
+public abstract class Entity {
+    private Polygon entity;
+    private Point2D movementVector;
+    private static final double accFactor = 0.03;
+
+    public Entity(int x, int y, Polygon entity){
+        this.entity = entity;
+        this.entity.setTranslateX(x);
+        this.entity.setTranslateY(y);
+        this.movementVector = new Point2D(0, 0);
+        this.entity.setFill(Color.WHITE);
+    }
+
+    public Polygon getEntity(){
+        return this.entity;
+    }
+
+    public void rotateRight(){
+        this.entity.setRotate(this.entity.getRotate() + 3);
+    }
+
+    public void rotateLeft(){
+        this.entity.setRotate(this.entity.getRotate() - 3);
+    }
+
+    public void renderEntity(Pane pane, Polygon entity){
+        pane.getChildren().add(entity);
+    }
+
+    public void moveEntity(double height, double width){
+        this.entity.setTranslateX(this.entity.getTranslateX() + this.movementVector.getX());
+        this.entity.setTranslateY(this.entity.getTranslateY() + this.movementVector.getY());
+
+        if(this.entity.getTranslateX() < 0)  this.entity.setTranslateX(width);
+        if(this.entity.getTranslateX() > width)  this.entity.setTranslateX(0);
+        if(this.entity.getTranslateY() < 0)  this.entity.setTranslateY(height);
+        if(this.entity.getTranslateY() > height)  this.entity.setTranslateY(0);
+        System.out.println(height + " " + width);
+    }
+
+    public void accelerate(){
+        double deltaX = Math.cos(Math.toRadians(entity.getRotate()));
+        double deltaY = Math.sin(Math.toRadians(entity.getRotate()));
+
+        deltaX *= accFactor;
+        deltaY *= accFactor;
+
+        this.movementVector = this.movementVector.add(deltaX, deltaY);
+    }
+}
---
+++ src/main/java/com/example/main/App.java
@@ -0,0 +1,38 @@
+package com.example.main;
+
+import com.example.character.Ship;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+
+/**
+ * Asteroids made with JavaFX by Dan Rusu
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        scene = new Scene(pane);
+        Ship ship = new Ship(400, 400);
+
+        pane.setPrefSize(800, 800);
+        pane.setStyle("-fx-background-color: #000000");
+        
+        ship.renderEntity(pane, ship.getEntity());
+        ship.handleControls(scene, ship, 800, 800);
+
+        stage.setTitle("Asteroids by Dan Rusu");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
